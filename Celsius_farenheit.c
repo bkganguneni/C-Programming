140d5fd9836bd7edcc3d236556ad7687a18bceea
BALAKAUSHIK GANGUNENI <51963897+bkganguneni@users.noreply.github.com>
2022-08-03 11:42:37 +0530

    Create Celsius_farenheit.c

---
+++ Celsius_farenheit.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+double ctof(int);
+double ftoc(int);
+
+int main(void) {
+	
+	int usertemp;
+	char unit;
+	double convertedtemp;
+	
+	scanf("%d %c", &usertemp, &unit);
+	if (unit=='C'){
+		
+		convertedtemp= ctof(usertemp);
+		
+		printf("%.1lf F",convertedtemp);
+		
+	} else if (unit=='F'){
+		
+		convertedtemp = ftoc(usertemp);
+		
+		printf("%.1lf C",convertedtemp);
+		
+		
+	}
+	
+	return 0;
+	
+}
+
+double ctof(int x){
+	return((9.0/5)*x+32);
+}
+
+double ftoc(int x){
+	return(5.0/9*(x-32));
+}
